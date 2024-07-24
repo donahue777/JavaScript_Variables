06b2b1780b9e23c6835d5864188aee758e1d8c64
Thomas Donahue <donahue777@yahoo.com>
2024-07-23 20:05:52 -0400

    Exercises Complete

--- app.js
+++ app.js
@@ -4,7 +4,44 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Thomas"; // Exercise 1 and 2, const declarations must be initialized.
+
+let lastName;
+
+var age;
 
 // Exercise 2
+lastName = "Donahue";
+
+age = 30;
 
 // Exercise 3
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
